47b96f5a940ff060729c08dd019f048aa3840499
Sawantkun <kumar.sawant37@gmail.com>
2022-11-22 00:13:16 +0530

    added features

--- src/components/Tiles/Tile.jsx
+++ src/components/Tiles/Tile.jsx
@@ -1,20 +1,40 @@
 import React from 'react'
+import { useState } from 'react'
 import "./Tile.css"
 import trash from "./assets/trash.svg"
 
 const Tile = (props) => {
+  const[searchValue,setsearchValue] = useState("Search Tasks");
+  const handleChange=(e)=>{
+    setsearchValue(e.target.value);
+    itemList.filter((item)=>{
+      if (item ===  e.target.value){
+        console.log(item);
+      }
+    })
+  }
+  const clear=()=>{
+    setsearchValue("");
+  }
     const deleteItem=key=>{
      const newlist = props.itemList.filter(itemObj=>{
         return itemObj.key !== key;
       });
       props.setItemList(newlist);
     };
+
   return (
       <div className='tasks-sec'>
         <h1>All Tasks</h1>
+        <input type="Search" value={searchValue} onChange={handleChange} onClick={clear}/>
         <div className='tasks'>
           {props.itemList.map(itemObj=>{
-            return <p className='tile'>{itemObj.item} <button onClick={()=>deleteItem(itemObj.key)}><img className='trash' src={trash} alt="" /></button></p>
+            return <div className='tile'>
+                     {itemObj.item} 
+                     <button onClick={()=>deleteItem(itemObj.key)}>
+                       <img className='trash' src={trash} alt="" />
+                     </button>
+                   </div>
           })}
         </div>
       </div>
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,25 +1,68 @@
 import React from 'react'
-import {useState} from 'react';
+import {useEffect,useState} from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 import Tile from '../Tiles/Tile';
 import "./Todo.css";
 
 const Input = () => {
   const[currentItem,setcurrentItem] = useState("");
   const[itemList,setItemList] = useState([]);
+  useEffect(()=>{
+    let arr = localStorage.getItem("tasklist");
+    if(arr){
+      let obj =JSON.parse(arr);
+      setItemList(obj);
+    }
+  },[])
+
   const handleChange = (e)=>{
     setcurrentItem(e.target.value);
   }
   const addItem = ()=>{
     setItemList([...itemList,{item:currentItem,key:Date.now()}]);
+    localStorage.setItem("tasklist", JSON.stringify(itemList));
     setcurrentItem("");
+    handleClose();
   }
+  const style = {
+    position: 'absolute' ,
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 420,
+    bgcolor: 'black',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    color:'white',
+  };
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return(
      <div>
       <div className='main'>
         <h1>Todo List</h1>
         <div className='todo'>
-          <input type="text" value={currentItem} onChange={handleChange}/>
-          <button id='button' onClick={addItem}>Add</button>
+          <button id='button2' onClick={handleOpen}>Add Tasks</button>
+          <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+        <p>Task Name</p>
+        <input id='modalinput' type="text" value={currentItem} onChange={handleChange}/>
+        <div className="buttons">
+          <button id='button2' onClick={handleClose}>Cancel</button>
+          <button id='button' onClick={addItem}>Add Task</button>
+        </div>
+        </Box>
+      </Modal>
         </div>
       </div>
         <Tile itemList={itemList} setItemList={setItemList}/>
